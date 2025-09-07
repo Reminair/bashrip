0a3a87b43a84055f943c8b79c3a5b64fd71520e8
Reminair <reminair@protonmail.com>
2025-09-07 17:45:04 -0400

    cleanup

--- bashrip.sh
+++ bashrip.sh
@@ -1,7 +1,7 @@
 # TODO:
 # * add iso extraction via mounting & copying or 7zip; then just delete the iso (prob a better way to do tho?)
 # * somehow get ddrescue to abort after a certain percentage successfully ripped
-# * improve the logic at 35-40 as it feels a little messy/unoptimised
+# * improve the ddrescue logic as it feels a little messy/unoptimised
 
 read -p "BarID: " barid
 read -p "DBase: " dbase
@@ -12,32 +12,35 @@ read -p "Multi? " multi
 read -p "2Side? " sides
 
 dpath="/dev/sr$dnum"
-final="$barid [$dbase-$dbid"                            # Begin constructing final name
-flagList=(                                              # ddrescue flags per stage
+tmpdir=$(mktemp -d --tmpdir=./)
+final="$barid [$dbase-$dbid"    # Begin constructing final name
+flagList=(                      # ddrescue flags per stage
     "-nv"
-    "-dvr 3"
-    "-Rdvr 3"
+    "-dv"
+    "-Rdv"
 )
 
-if [[ "${multi,,}" =~ ^(yes|y)$ ]]; then                # Adds disc number if given
+if [[ "${multi,,}" =~ ^(yes|y)$ ]]; then    # Adds disc number if given
     read -p " Disc# " discnum
     final+=",disc-$discnum"
 fi
-if [[ "${sides,,}" =~ ^(yes|y)$ ]]; then                # Adds disc side if given
+if [[ "${sides,,}" =~ ^(yes|y)$ ]]; then    # Adds disc side if given
     read -p " Side# " discside
     final+=",side-$discside"
 fi
 
-final+="]"                                              # Wraps up final name with a closing bracket
-dvdbackup -Mpi $dpath -n $barid -ra                     # Attempts to backup entire disc, aborts on error
-exstat=$?                                               # Records exit code
+final+="]"                                      # Wraps up final name with a closing bracket
+dvdbackup -Mpi $dpath -o $tmpdir -n disc -ra    # Attempts to backup entire disc, aborts on error
+exstat=$?                                       # Records exit code
 
-if [[ "$exstat" -ne '0' ]]; then                        # Starts ddrescue if aborted
-    for flags in "${flagList[@]}"; do                   # Loops ddrescue 3 times with flaglist
-        ddrescue -b 2048 $flags $dpath $barid.iso $barid.log
-        exstat=$?
+if [[ "$exstat" -ne '0' ]]; then                                                  # Checks if dvdbackup aborted
+    rm -r "$tmpdir/disc"                                                          # Removes dvdbackup output
+    for flags in "${flagList[@]}"; do                                             # Loops ddrescue 3 times, incrementing flaglist
+        ddrescue -b 2048 $flags $dpath "$tmpdir/disc.iso" "$tmpdir/rescue.log"    # Runs ddrescue with flags from flaglist array
     done
+    mv "$tmpdir/disc.iso" "$final.iso"
+else
+    mv "$tmpdir/disc" "$final"
 fi
 
-mv $barid "$final"                                      # Append identifiers to output
-eject $dpath                                            # Eject disc and terminate program
+eject $dpath    # Eject disc and terminate program
